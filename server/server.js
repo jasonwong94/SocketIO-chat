a707a902923048ffabb4e7a97e2304e1dae70ed2
Jason Wong <jaw.wong@mail.utoronto.ca>
2015-09-12 00:04:44 -0400

    Added socket.io and reorganized structure of repo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 var app = require( 'express' )();
 var http = require( 'http').Server( app );
+var io= require( 'socket.io')(http);
 
 var PORT_NUMBER = 4400;
 
 app.get('/', function(req, res){
 	res.sendFile( __dirname + '/sample.html');
+});
+
+io.on( 'connection', function(socket){
+	console.log('a user connected');
 });
 
 http.listen( PORT_NUMBER, function(){
