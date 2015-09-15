e8a549617287d7d19a7b75aa059553d06791e36a
Jason Wong <jaw.wong@mail.utoronto.ca>
2015-09-15 02:27:00 -0400

    Log-in feature to allow user to identify who they are in chat

--- client/client.js
+++ client/client.js
@@ -7,16 +7,29 @@ app.controller( 'clientController', clientController );
 
 function clientController(){
 	var vm = this;
+	vm.Username = "";
+	vm.FormComplete = false;
 
 	vm.send = send;
+	vm.setComplete = setComplete;
+
 	function send(){
 		console.log( vm.chat );
 		socket.emit( 'message', vm.chat );
+
+		//reset
+		vm.chat = undefined;
 
 		//prevent button from refreshing page
 		return false;
 	};
 
+	function setComplete(){
+		vm.FormComplete = true;
+		console.log( vm.Username );
+	}
+
+	//listen for any incoming messages
 	socket.on('server', function( msg ){
 		console.info( 'received from server: '+ msg );
 	});
